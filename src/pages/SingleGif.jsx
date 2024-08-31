ca8ab6d0f5cc107af410fc57c2e7f707c0f959b8
SohamKale <113749828+Soham-Kale@users.noreply.github.com>
2024-08-31 22:44:07 +0530

    Update SingleGif.jsx

--- src/pages/SingleGif.jsx
+++ src/pages/SingleGif.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { GifState } from "../context/gif-context";
-import Gif from "../components/gif";
+import Gif from "../components/Gif";
 import FollowOn from "../components/FollowOn";
 
 import { HiOutlineExternalLink } from "react-icons/hi";
